fdc8d2c697754be9f0e5a18ea97f54d3accf3fbf
Thiago <133411658+FtcThiago@users.noreply.github.com>
2025-06-07 16:18:57 -0300

    Create conclusao.py

---
+++ Paginas_de_interacao/conclusao.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def exibir():
+    st.title("🧠 Conclusões")
+    st.markdown("""
+    - ✅ **Ponto positivo:** (ex: crescimento de concluintes em universidades públicas)
+    - ⚠️ **Alerta:** (ex: evasão maior entre pessoas negras ou em instituições privadas)
+    - 💡 **Recomendações:** (ex: foco em políticas para permanência escolar)
+
+    Escreva suas observações com base nos dados.
+    """)
