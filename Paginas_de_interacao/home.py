d3a5326db72563e58dbd4177fb515ac1ff7e17c3
Thiago <133411658+FtcThiago@users.noreply.github.com>
2025-06-07 16:19:06 -0300

    Create home.py

---
+++ Paginas_de_interacao/home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def exibir():
+    st.title("🏠 Bem-vindo ao Projeto de Análise Educacional")
+    st.markdown("""
+    Este aplicativo tem como objetivo responder perguntas investigativas sobre o cenário educacional brasileiro
+    e analisar as taxas de evasão com base em dados reais.
+
+    **O que você vai encontrar aqui:**
+    - Gráficos e tabelas sobre matrícula e conclusão
+    - Análises por sexo, raça, e ano
+    - Estimativas e cálculos da evasão escolar
+    - Um painel interativo para você explorar os dados
+
+    ---
+    """)
