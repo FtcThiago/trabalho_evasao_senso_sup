5649dd888a4260fae0efabfcaf5ee0f723f4ba62
Thiago <133411658+FtcThiago@users.noreply.github.com>
2025-06-09 20:55:16 -0300

    mudando de vez

--- Paginas_de_interacao/home.py
+++ Paginas_de_interacao/home.py
@@ -19,37 +19,37 @@ def exibir():
 
 
     # Propósito da Análise
-    st.markdown("""
-    ## 🎯 Propósito da Análise
+        st.markdown("""
+        ## 🎯 Propósito da Análise
 
-    Este projeto tem como objetivo compreender os fatores relacionados à **evasão no ensino superior** do Distrito Federal.
+        Este projeto tem como objetivo compreender os fatores relacionados à **evasão no ensino superior** do Distrito Federal.
 
-    Através dos dados públicos do Censo da Educação Superior, buscamos responder perguntas como:
+        Através dos dados públicos do Censo da Educação Superior, buscamos responder perguntas como:
 
-    - Quantos alunos ingressaram e quantos se formaram ao longo dos anos?
-    - Existe diferença de evasão entre cursos noturnos e diurnos?
-    - A evasão varia entre faixas etárias?
-    - A cor/raça influencia na taxa de evasão?
-    - Alunos com deficiência têm maior evasão?
-    - A evasão é diferente entre cursos de áreas diferentes?
-    - A evasão é maior em cursos EAD?
-    - Qual é a distribuição racial dos estudantes em cada área do ensino superior?
+        - Quantos alunos ingressaram e quantos se formaram ao longo dos anos?
+        - Existe diferença de evasão entre cursos noturnos e diurnos?
+        - A evasão varia entre faixas etárias?
+        - A cor/raça influencia na taxa de evasão?
+        - Alunos com deficiência têm maior evasão?
+        - A evasão é diferente entre cursos de áreas diferentes?
+        - A evasão é maior em cursos EAD?
+        - Qual é a distribuição racial dos estudantes em cada área do ensino superior?
 
 
-    """)
+        """)
 
-    # Créditos
-    st.markdown("---")
-    st.markdown("""
-    👨‍💻 **Alunos:** Thiago Félix, Pedro Guimarães  
-    👨‍🏫 **Professor orientador:** Alexandre Roriz  
-    📅 **Ano dos dados analisados:** 2018-2022
-    """)
+        # Créditos
+        st.markdown("---")
+        st.markdown("""
+        👨‍💻 **Alunos:** Thiago Félix, Pedro Guimarães  
+        👨‍🏫 **Professor orientador:** Alexandre Roriz  
+        📅 **Ano dos dados analisados:** 2018-2022
+        """)
 
-    # Chamada final
-    st.markdown("---")
-    st.markdown("""
-    <h2>👨‍💻 <b>Alunos:</b> Thiago Félix, Pedro Guimarães</h2>
-    <h2>👨‍🏫 <b>Professor orientador:</b> Alexandre Roriz</h2>
-    <h2>📅 <b>Ano dos dados analisados:</b> 2018-2022</h2>
-""", unsafe_allow_html=True)
+        # Chamada final
+        st.markdown("---")
+        st.markdown("""
+        <h2>👨‍💻 <b>Alunos:</b> Thiago Félix, Pedro Guimarães</h2>
+        <h2>👨‍🏫 <b>Professor orientador:</b> Alexandre Roriz</h2>
+        <h2>📅 <b>Ano dos dados analisados:</b> 2018-2022</h2>
+    """, unsafe_allow_html=True)
